1368dec149a2d3fdf13cb4624406471af715548b
Ari Julianto <arijulianto@programmer.net>
2017-04-07 09:07:48 +0700

    add more site config

--- _system/config/website.php
+++ _system/config/website.php
@@ -15,15 +15,21 @@ define('MEDIA_URL', 'http://'.DOMAIN.(SITE_DIR ? '/'.SITE_DIR : '').'/media/');
 define('MEDIA_URI', (SITE_DIR ? '/'.SITE_DIR : '').'/media/');
 define('MEDIA_DIR', BASEPATH.'_media/');
 
+/* Environment & Local Server Setting */
+$conf['debug_level'] = 2; // option: 0, 1, 2
+$conf['locale_code'] = 'id_ID';
+$conf['timezone'] = 'Asia/Jakarta';
+
+
+
 /* Default Setting */
 $conf['site_name'] = 'AJN Framework';
-$conf['site_open'] = true;
+$conf['site_status'] = 'online'; // option: online, maintenance, offline
 $conf['site_login'] = false;
 
+$conf['404_mode'] = 'standalone'; // option: standalone, full
 $conf['default_module'] = 'home';
-$conf['autoload_module'] = true;
-
+$conf['autoload_module'] = false;
 $conf['default_paging'] = 10;
-
 $conf['db_setting'] = false;
 
