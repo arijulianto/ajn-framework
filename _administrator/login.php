ce428a7731cd7dfdd6897eb334bc9bacef0fc54a
Ari Julianto <arijulianto@programmer.net>
2017-09-20 17:21:21 +0700

    Updated Method for Administrator

---
+++ _administrator/login.php
@@ -0,0 +1,5 @@
+login page
+<form action="<?php echo ADMIN_URI ?>login" method="post">
+<p><input type="email" name="user_login" placeholder="Email" /></p>
+<p><input type="password" name="user_password" placeholder="Password" /></p>
+<p><input type="submit" value="Login" /></p>
