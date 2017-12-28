f9568260f2c5d81ce8d520d94ca024ed9d1fae20
Ari Julianto <arijulianto@programmer.net>
2017-12-28 13:28:48 +0700

    Update upload
    * add upload base64 to image
    * fixed bug resize PNG image for transparent background
    * add crop method
    * fix bug upload stability

--- _plugin/class.upload.php
+++ _plugin/class.upload.php
@@ -5,38 +5,34 @@ Plugin UPLOAD GAMBAR
 ======================
 Oleh: Ari Julianto
 Dibuat: Desember 2014
-Update: Mei 2017
+Update: Oktober 2017
 
 
 == Cara Penggunaan ==
 if($_FILES['file']['size']>0){
-    $upload = new upload($_FILES['NAMA_INPUT_GAMBAR']); // parameter: new upload(source, location) -- source: required location: ''
-    $upload->add($_FILES['NAMA_INPUT_GAMBAR']); // parameter: add(source, location) -- source: required location: '' ~ alternative
-    $upload->upload(); // parameter: upload(newName,maxSize) -- newName: default maxSize: default
-    $upload->resize(800); // parameter: resize(maxWidth, newFilename) -- default param: null
-    $upload->thumbnail(); // parameter: thumbnail(maxWidth, prefix); -- default: maxWidth=50 prefix:t_
+    $upload = new upload($_FILES['NAMA_INPUT_GAMBAR'], MEDIA_PATH.'images'); // parameter: new upload(source, location) -- source: required location: ''
+    //$upload->add($_FILES['NAMA_INPUT_GAMBAR']); // parameter: add(source, location) -- source: required location: '' ~ alternative
+    $hasil_upload = $upload->upload(null, 1024); // parameter: upload(newName,maxSize) -- newName: default maxSize: default
+    //$upload->resize(800); // parameter: resize(maxWidth, newFilename) -- default param: null
+    $upload->thumbnail(300, 'a_'); // parameter: thumbnail(maxWidth, prefix); -- default: maxWidth=50 prefix:t_
 }
 */
 
 class upload{
     public $source = null;
-    public $maxWidth = 400;
-    public $maxHeight = 400;
-    public $maxDimension = 400;
+    public $maxDimension = 500;
     public $thumbDimension = 50;
-    public $path = null;
-    public $fullpath = null;
-    public $name = null;
+    public $path = '';
+    public $fullpath = '';
+    public $name = '';
 
     function __construct($source='',$dir=''){
         if($source){
             $this->source = $source;
             $this->name = $source['name'];
         }
-        if($dir){
-            $this->path = $dir;
-            $this->fullpath = ($dir ? $dir.'/' : '') . $this->name;
-        }
+        $this->path = $dir;
+        $this->fullpath = ($dir ? $dir.'/' : '') . $this->name;
     }
 
     function add($source,$dir=''){
@@ -45,19 +41,27 @@ class upload{
         $this->name = $source['name'];
         $this->fullpath = ($dir ? $dir.'/' : '') . $this->name;
     }
-    
+
     function upload($name=null,$resize=0){
         if($name){
-            $this->name = $name;
-            $this->fullpath = ($this->path ? $this->path.'/' : '') . $name;
+            $exts = array('jpg'=>'jpg','jpeg'=>'jpg','png'=>'png','gif'=>'gif');
+            $ext = end(explode('.',strtolower($name)));
+            if($exts[$ext]){
+                $this->name = $name;
+            }else{
+                $ext = end(explode('.',strtolower($this->name)));
+                $this->name = $name.'.'.$ext;
+            }
+            $this->fullpath = ($this->path ? $this->path.'/' : '') . $this->name;
         }
         if($this->source && $this->name){
+            $finfo = getimagesize($this->source['tmp_name']);
             $upload = move_uploaded_file($this->source['tmp_name'], $this->fullpath);
             if($upload){
-                return $this->name;
-                if($resize>0){
-                   $this->resize($resize);
+                if($resize>0 && ($resize<$finfo[0] || $resize<$finfo[1])){
+                    $this->resize($resize);
                 }
+                return $this->name;
             }else{
                 die('<div class="warning">Upload failed, please try again!</div>');
             }
@@ -65,56 +69,140 @@ class upload{
             die('<div class="warning">Upload failed. Please set source and target name file!</div>');
         }
     }
-    
+
     function resize($max, $imgFile=null) {
         $originalFile = $this->fullpath;
         $imgFile = ($this->path ? $this->path.'/' : '') . ($imgFile ? $imgFile : $this->name);
-        
         $info = getimagesize($originalFile);
         $mime = $info['mime'];
-    
         switch ($mime) {
-                case 'image/jpg':
-                case 'image/jpeg':
-                        $image_create_func = 'imagecreatefromjpeg';
-                        $image_save_func = 'imagejpeg';
-                        break;
-    
-                case 'image/png':
-                        $image_create_func = 'imagecreatefrompng';
-                        $image_save_func = 'imagepng';
-                        break;
-    
-                case 'image/gif':
-                        $image_create_func = 'imagecreatefromgif';
-                        $image_save_func = 'imagegif';
-                        break;
-    
-                default:
-                        die('Unknown image type.');
+            case 'image/jpg':
+            case 'image/jpeg':
+            $image_create_func = 'imagecreatefromjpeg';
+            $image_save_func = 'imagejpeg';
+            break;
+            case 'image/png':
+            $image_create_func = 'imagecreatefrompng';
+            $image_save_func = 'imagepng';
+            break;
+            case 'image/gif':
+            $image_create_func = 'imagecreatefromgif';
+            $image_save_func = 'imagegif';
+            break;
+            default:
+            die('Unknown image type.');
         }
-    
         $img = $image_create_func($originalFile);
         list($width, $height) = getimagesize($originalFile);
-        
         $newDimension = $this->calculateDimensions($width,$height,$max);
         $newWidth = $newDimension['width'];
         $newHeight = $newDimension['height'];
-        
         $tmp = imagecreatetruecolor($newWidth, $newHeight);
+        if($mime=='image/png'){
+            imagealphablending($tmp, false);
+            imagesavealpha($tmp, true);
+        }
         imagecopyresampled($tmp, $img, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-        
         $image_save_func($tmp, $imgFile);
         imagedestroy($tmp);
     }
-    
+
+
+
+    function crop($name=null, $width = 200, $height = 200){
+        $quality = 70;
+        if($name){
+            $exts = array('jpg'=>'jpg','jpeg'=>'jpg','png'=>'png','gif'=>'gif');
+            $ext = end(explode('.',strtolower($name)));
+            if($exts[$ext]){
+                $this->name = $name;
+            }else{
+                $ext = end(explode('.',strtolower($this->name)));
+                $this->name = $name.'.'.$ext;
+            }
+            $this->fullpath = ($this->path ? $this->path.'/' : '') . $this->name;
+        }
+        $image_data = getimagesize( $this->source['tmp_name'] );
+        switch($image_data['mime']){
+            case 'image/gif':
+            $get_func = 'imagecreatefromgif';
+            $suffix = ".gif";
+            break;
+            case 'image/jpeg';
+            $get_func = 'imagecreatefromjpeg';
+            $suffix = ".jpg";
+            break;
+            case 'image/png':
+            $get_func = 'imagecreatefrompng';
+            $suffix = ".png";
+            break;
+        }
+        $img_original = call_user_func( $get_func, $this->fullpath );
+        $old_width = $image_data[0];
+        $old_height = $image_data[1];
+        $new_width = $width;
+        $new_height = $height;
+        $src_x = 0;
+        $src_y = 0;
+        $current_ratio = round( $old_width / $old_height, 2 );
+        $desired_ratio_after = round( $width / $height, 2 );
+        $desired_ratio_before = round( $height / $width, 2 );
+        $new_image = imagecreatetruecolor( $width, $height );
+        if( $current_ratio > $desired_ratio_after ){
+            $new_width = $old_width * $height / $old_height;
+        }
+        if( $current_ratio > $desired_ratio_before && $current_ratio < $desired_ratio_after ){
+            if( $old_width > $old_height ){
+                $new_height = max( $width, $height );
+                $new_width = $old_width * $new_height / $old_height;
+            }
+            else {
+                $new_height = $old_height * $width / $old_width;
+            }
+        }
+        if( $current_ratio < $desired_ratio_before  ){
+            $new_height = $old_height * $width / $old_width;
+        }
+        $width_ratio = $old_width / $new_width;
+        $height_ratio = $old_height / $new_height;
+        $src_x = floor( ( ( $new_width - $width ) / 2 ) * $width_ratio );
+        $src_y = round( ( ( $new_height - $height ) / 2 ) * $height_ratio );
+        imagecopyresampled( $new_image, $img_original, 0, 0, $src_x, $src_y, $new_width, $new_height, $old_width, $old_height );
+        imagejpeg( $new_image, $this->fullpath, $quality  );
+        imagedestroy( $new_image );
+        imagedestroy( $img_original );
+        return true;
+    }
+
+    function cropImage($source, $width=100, $height=100, $fileName=false){
+        $thumb_img  =   imagecreatefromjpeg($source);
+        $fileName = $fileName ? $fileName : $source;
+        list($w, $h) = getimagesize($source);
+        if($w > $h) {
+            $new_height =   $height;
+            $new_width  =   floor($w * ($new_height / $h));
+            $crop_x     =   ceil(($w - $h) / 2);
+            $crop_y     =   0;
+        }else{
+            $new_width  =   $width;
+            $new_height =   floor( $h * ( $new_width / $w ));
+            $crop_x     =   0;
+            $crop_y     =   ceil(($h - $w) / 2);
+        }
+
+        $tmp_img = imagecreatetruecolor($width,$height);
+        imagecopyresampled($tmp_img, $thumb_img, 0, 0, $crop_x, $crop_y, $new_width, $new_height, $w, $h);
+        imagejpeg($tmp_img,$fileName,80);
+        imagedestroy($tmp_img);
+    }
+
     function thumbnail($dimension=null,$prefix=null) {
         $targetFile = ($prefix ? $prefix : 't_') . $this->name;
         $dimension = $dimension ? $dimension : $this->thumbDimension;
         $this->resize($dimension,$targetFile);
     }
-    
-    
+
+
     function base64_to_image($base64_string, $output_filename, $path=null) {
         $exts = array('jpg'=>'jpg','jpeg'=>'jpg','png'=>'png','gif'=>'gif');
         $data = explode(';base64,', $base64_string);
@@ -128,7 +216,14 @@ class upload{
         fclose($ifp); 
         return $path.$output_filename;
     }
-    
+
+    function image_to_base64($file=null){
+        $image = $file ? $file : $this->fullpath;
+        $type = pathinfo($image, PATHINFO_EXTENSION);
+        $data = file_get_contents($image);
+        return 'data:image/' . $type . ';base64,' . base64_encode($data);
+    }
+
     function calculateDimensions($width,$height,$max){
         $ratio = $width/$height;
         if($ratio > 1){
