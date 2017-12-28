610c60a730cfd482da097b7cce165ef7f48fe711
Ari Julianto <arijulianto@programmer.net>
2017-12-28 13:46:20 +0700

    Update index.php

--- _module/page/index.php
+++ _module/page/index.php
@@ -1,7 +1,1 @@
-<div class="page-transition-scaleUpCenter">
-<p><input type="number" value="ini put text" /></p>
-<p><label class="input">ini put text rasj,rbh arjbhas,jrfb as, bas,fbasf,asb f,asbf ,asfb as,mfbas ,f<br />ghjkgn jk jkhng jkgn kj blsdkjfh g;dfjgh dlfjgh dfl;gh fdlkh <br />khg ldfhkg lksdfg lsdkjg sdklj </label></p>
-
-
-<div class="animated fadeOut infinite">dadadad</div>
-</div>
+Page Content
