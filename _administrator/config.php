45df1a26976765468c878408992f926d7be77371
Ari Julianto <arijulianto@programmer.net>
2018-01-12 10:55:14 +0700

    Update config.php

--- _administrator/config.php
+++ _administrator/config.php
@@ -1,6 +1,6 @@
 <?php
 $adm['login_source'] = 'tbl_admin';
 $adm['login_session'] = array('uid'=>'idadmin', 'nama'=>'nama');
-$adm['login_cek'] = array('user_login'=>'user_email', 'user_password'=>'user_password');
+$adm['login_cek'] = array('user_email', 'user_password');
 $adm['login_encrypt'] = 'md5+base64';
 $adm['login_recent'] = false;
