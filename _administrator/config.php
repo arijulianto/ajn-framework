21d5e48fb3d0852e1fd69477be28a4d419f81950
Ari Julianto <arijulianto@programmer.net>
2018-07-19 17:54:59 +0700

    penambahan fitur force login untuk Admin

--- _administrator/config.php
+++ _administrator/config.php
@@ -1,9 +1,9 @@
 <?php
 $adm['login_id'] = 'idadmin';
-//$adm['login_source'] = array('username'=>'admin', 'password'=>'admin');
-//$adm['login_session'] = array('uid'=>'999', 'username'=>'admin', 'nama'=>'Admin Test', 'level'=>'999');
-$adm['login_source'] = 'tbl_admin';
-$adm['login_session'] = array('uid'=>'idadmin', 'nama'=>'nama', 'level'=>'levelnum');
+$adm['login_source'] = array('username'=>'admin', 'password'=>'admin');
+$adm['login_session'] = array('uid'=>'999', 'username'=>'admin', 'nama'=>'Admin Test', 'level'=>'999');
+//$adm['login_source'] = 'tbl_admin';
+//$adm['login_session'] = array('uid'=>'idadmin', 'nama'=>'nama', 'level'=>'levelnum');
 $adm['login_cek'] = array('user_email', 'user_password');
 $adm['login_encrypt'] = 'md5+base64';
 $adm['login_recent'] = true;
